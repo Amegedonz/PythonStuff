a8977b240c00f40befc177131b0a5f6989707135
Lucian <lucian.m.ho@gmail.com>
2022-07-10 22:04:27 +0800

    first problem set of Pset 0

---
+++ Pset0/indoor.py
@@ -0,0 +1,16 @@
+#This Pset requires me to take an input and output in lowercase 
+#Im going to tackle this with an input and loop it through ASCII
+
+#String = str(input("Input words here: "))
+#Lowercase_String = String.casefold()
+#print(Lowercase_String)
+
+String = str(input("Input words here: "))
+Split_word = list(String)
+
+if ord(Split_word) = > 41 and <= 90:
+    lower_case = char(ord(Split_word)+32)
+    print(lower_case)
+
+else :
+    print(Split_word)
